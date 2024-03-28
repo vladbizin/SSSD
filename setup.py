426c393b2bfbc496702da0688c38d5591306422b
Vladislav Bizin <vlad.bizin2001@gmail.com>
2024-03-28 12:44:43 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   license="MIT",
   long_description=readme(),
   long_description_content_type='text/markdown',
-  packages=find_packages(exclude=['test_example']),
+  packages=find_packages(exclude=["tests"]),
   include_package_data=True,
   url='',
   install_requires=[
